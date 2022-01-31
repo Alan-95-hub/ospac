087c724059567f6170bf81b6f809fbb715b4f072
Alan-95-hub <aibishaev@edu.hse.ru>
2022-01-31 14:19:38 +0300

    Lab 3 done.

---
+++ task 3-2.c
@@ -0,0 +1,24 @@
+﻿
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+int main()
+{
+    pid_t pid, ppid;
+    int a = 0;
+    (void)fork();
+    a = a + 1;
+    pid = fork();
+    if (pid == -1) {
+        printf("Error, ");
+    }
+    else if (pid == 0) {
+        printf("Child, ");
+    }
+    else {
+        printf("Parent, ");
+    }
+    ppid = getppid();
+    printf("My pid = %d, my ppid = %d", (int)pid, (int)ppid);
+        return 0;
+}
