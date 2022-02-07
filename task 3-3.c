8874d1ec0120b029687a4ffd62b4cdbcb3117cae
Alan-95-hub <aibishaev@edu.hse.ru>
2022-02-07 13:35:22 +0300

    Lab 4 done.

---
+++ task 3-3.c
@@ -0,0 +1,15 @@
+﻿#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+int main(int argc, char* argv[], char* envp[]) {
+	int i = 0;
+	for (; i < argc; ++i) {
+		printf("argv - %s\n", argv[i]);
+	}
+	i = 0;
+	while (envp[i]) {
+		printf("envp - %s\n", envp[i]);
+		i++;
+	}
+	return 0;
+}
---
+++ task 3-4.c
@@ -0,0 +1,19 @@
+﻿#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(int argc, char* argv[], char* envp[]) {
+	pid_t pid = fork();
+	if (pid < 0) {
+		printf("Error\n");
+	}
+	else if (pid == 0) {
+		printf("Child\n");
+		(void)execle("/bin/cat", "/bin/cat", "task4-1.c", 0, envp);
+		printf("Error\n");
+	}
+	else printf("Parent\n");
+	pid_t ppid = getppid();
+	printf("My pid = %d, my ppid = %d", (int)pid, (int)ppid);
+	return 0;
+}
